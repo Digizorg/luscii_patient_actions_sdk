edf542e836b1a2d05adbb1d80a99a8b935538adb
Ruben <r.vandenengel@erasmusmc.nl>
2024-11-21 11:35:09 +0100

    feat: Include Android LusciiFlutterSdkError

---
+++ luscii_patient_actions_sdk_android/android/src/main/kotlin/nl/digizorg/errors/LusciiFlutterSdkError.kt
@@ -0,0 +1,8 @@
+package nl.digizorg.errors
+
+enum class LusciiFlutterSdkError(val code: String, val message: String) {
+    UNKNOWN("0", "Unknown error"),
+    INVALID_ARGUMENTS("1", "Invalid arguments"),
+    INVALID_API_KEY("2", "Invalid API Key"),
+    UNAUTHORIZED("3", "Unauthorized");
+}
